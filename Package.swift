54e8ebc3b6b9ec2ae75cf1c4eaa31239e4f2682c
Ibrahim Qraiqe <ibrahim@m360.no>
2019-11-24 01:58:25 +0100

    changed source for Realm and Stencil dependicity

--- Package.swift
+++ Package.swift
@@ -17,18 +17,18 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "git@github.com:SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.7.0")),
         .package(url: "git@github.com:Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
-        .package(url: "git@github.com:realm/realm-cocoa.git", .upToNextMajor(from: "3.18.0")),
+        .package(url: "git@github.com:realm/realm-cocoa.git", .upToNextMajor(from: "4.1.1")),
         .package(url: "git@github.com:ZamzamInc/ZamzamKit.git", .upToNextMajor(from: "5.1.0")),
-        .package(url: "git@github.com:ZamzamInc/Stencil.git", .branch("lite")),
+        //.package(url: "git@github.com:ZamzamInc/Stencil.git", .branch("lite")),
+        .package(url: "https://github.com/kylef/Stencil.git", from: "0.13.0"),
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.0"),
         .package(url: "git@github.com:onevcat/Kingfisher.git", .upToNextMajor(from: "5.8.1"))
     ],
     targets: [
         .target(
             name: "SwiftyPress",
             dependencies: [
-                "SwiftyBeaver",
                 "Alamofire",
                 "Realm",
                 "RealmSwift",
@@ -36,6 +36,7 @@ let package = Package(
                 "ZamzamNotification",
                 "ZamzamUI",
                 "Stencil",
+        "StencilSwiftKit",
                 "Kingfisher"
             ]
         ),
